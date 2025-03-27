b658c26e5372528b642ab59e792a7e831438fd0d
VaughanA-Uber <vaughan@uber.com>
2025-03-27 12:05:04 -0700

    update nvim config

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -210,30 +210,73 @@ return {
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         -- clangd = {},
-        -- gopls = {},
-        -- pyright = {},
+        gopls = {
+          cmd = { '/opt/homebrew/bin/gopls', '-remote=auto', '-rpc.trace', '-v' },
+          flags = {
+            debounce_text_changes = 1000,
+          },
+          keys = {
+            -- Workaround for the lack of a DAP strategy in neotest-go: https://github.com/nvim-neotest/neotest-go/issues/12
+            -- { "<leader>td", "<cmd>lua require('dap-go').debug_test()<CR>", desc = "Debug Nearest (Go)" },
+          },
+          settings = {
+            gopls = {
+              gofumpt = true,
+              codelenses = {
+                gc_details = false,
+                generate = true,
+                regenerate_cgo = true,
+                -- run_govulncheck = true,
+                test = true,
+                tidy = true,
+                upgrade_dependency = true,
+                vendor = true,
+              },
+              hints = {
+                assignVariableTypes = true,
+                compositeLiteralFields = true,
+                compositeLiteralTypes = true,
+                constantValues = true,
+                functionTypeParameters = true,
+                parameterNames = true,
+                rangeVariableTypes = true,
+              },
+              analyses = {
+                fieldalignment = true,
+                nilness = true,
+                unusedparams = true,
+                unusedwrite = true,
+                useany = true,
+              },
+              usePlaceholders = true,
+              completeUnimported = true,
+              staticcheck = true,
+              directoryFilters = {
+                '-bazel-bin',
+                '-bazel-out',
+                '-bazel-testlogs',
+                '-bazel-mypkg',
+                '-.git',
+                '-.vscode',
+                '-.idea',
+                '-.vscode-test',
+                '-node_modules',
+              },
+              semanticTokens = true,
+            },
+          },
+        },
+        pyright = {},
         -- rust_analyzer = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
-        --
+        -- tsserver = {},
+        -- html = { filetypes = { 'html', 'twig', 'hbs'} },
 
         lua_ls = {
-          -- cmd = { ... },
-          -- filetypes = { ... },
-          -- capabilities = {},
-          settings = {
-            Lua = {
-              completion = {
-                callSnippet = 'Replace',
-              },
-              -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-              -- diagnostics = { disable = { 'missing-fields' } },
-            },
+          Lua = {
+            workspace = { checkThirdParty = false },
+            telemetry = { enable = false },
+            -- NOTE: toggle below to ignore Lua_LS's noisy `missing-fields` warnings
+            -- diagnostics = { disable = { 'missing-fields' } },
           },
         },
       }
@@ -262,6 +305,10 @@ return {
         automatic_installation = false,
         handlers = {
           function(server_name)
+            if server_name == 'gopls' then
+              return
+            end
+
             local server = servers[server_name] or {}
             -- This handles overriding only values explicitly passed
             -- by the server configuration above. Useful when disabling
@@ -270,6 +317,16 @@ return {
             require('lspconfig')[server_name].setup(server)
           end,
         },
+      }
+
+      require('lspconfig').gopls.setup {
+        cmd = { '/opt/homebrew/bin/gopls', '-remote=auto', '-rpc.trace', '-v' },
+        flags = {
+          debounce_text_changes = 1000,
+        },
+        on_attach = on_attach,
+        settings = servers['gopls'],
+        filetypes = (servers['gopls'] or {}).filetypes,
       }
     end,
   },
